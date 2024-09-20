a6f0d99b26b1a15e6a9d002972c07afc506404d8
Serhii Krasovskyi <chokorik@gmail.com>
2024-09-20 13:27:04 +0300

    initial project

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(cors());
+
+  app.use(express.json());
+
+  app.listen(3000, () => {
+    console.log('Server running on port 3000!');
+  });
+};
